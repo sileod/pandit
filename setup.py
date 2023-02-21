fdb508d08a1bfa2c672d49acefc24356a544098d
sileod <damien.sileo@gmail.com>
2023-02-21 10:18:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='xpflow',
+      version='{{VERSION_PLACEHOLDER}}',
+      description='Utilities for representing experiments with classes',
+      url='https://github.com/sileod/xpflow',
+      author='sileod',
+      license='MIT',
+      install_requires=['pandas'],
+      py_modules=['pandit'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
