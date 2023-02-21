c20cf5beed859f78178786572abaf8fdbd30cb35
sileod <damien.sileo@gmail.com>
2023-02-21 10:51:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-setup(name='xpflow',
+setup(name='pandit',
       version='{{VERSION_PLACEHOLDER}}',
       description='Pandas with some cool additional features',
       url='https://github.com/sileod/pandit',
